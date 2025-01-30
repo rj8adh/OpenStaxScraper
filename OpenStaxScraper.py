a8678a6d25127417c790ed52ae6220df1a0057e7
rj8 <rj8adhikari@gmail.com>
2025-01-29 20:33:43 -0600

    Added saving data to json file

--- OpenStaxScraper.py
+++ OpenStaxScraper.py
@@ -1,14 +1,16 @@
 from UnitScraper import scrapeUnits
 from IndividualPageScraper import scrapePage
 from IndPageScrapNewline import scrapeWithNewlines
+import json
 
 textbookQuestPages = scrapeUnits(['Review Questions', 'Critical Thinking Questions', 'Test Prep for APÂ® Courses'], 'https://openstax.org/books/biology-ap-courses/pages/2-review-questions', 'https://openstax.org/books/biology-ap-courses/pages/')
 
 allQuestions = []
 
-for i in range(len(textbookQuestPages)):
+for i in range(len(allQuestions)):
     print(textbookQuestPages[i])
+    # If scrapePage doesn't work, try using scrapeWithNewlines, some textbooks are wonky
     allQuestions.append(scrapePage(textbookQuestPages[i]))
 
-for pageQuestions in allQuestions:
-    print(pageQuestions)
+with open("questionsAndAnswers.json", "w") as f:
+    json.dump(allQuestions, f)
